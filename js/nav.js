728f89488248aef80ae6f329f378b5a4da1b5377
Erika Becker <97013344+eribecke@users.noreply.github.com>
2024-09-03 13:20:37 -0400

    dropdown menu

--- js/nav.js
+++ js/nav.js
@@ -10,3 +10,11 @@ function closeNav() {
 
 document.querySelector('.openbtn').addEventListener('click', openNav);
 document.querySelector('.closebtn').addEventListener('click', closeNav);
+
+document.querySelectorAll('.dropdown-btn').forEach(button => {
+  button.addEventListener('click', function(event) {
+      event.preventDefault();
+      const dropdownContent = this.nextElementSibling;
+      dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+  });
+});

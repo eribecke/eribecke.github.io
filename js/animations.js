15db682724ffc4f9e4f4a5fd8b685f345fe918ca
eribecke <eribecke@iu.edu>
2026-03-25 20:45:33 -0400

    added cursor buddy

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,25 @@
 import { animate, createDraggable, spring } from 'https://esm.sh/animejs';
 
+
+const cursorBuddy = document.getElementById("buddy");
+let currX = 0;
+let currY = 0;
+const distanceOffset = -20;
+
+document.addEventListener('mousemove', e => {
+    cursorBuddy.style.transform = `translate(${e.clientX + (distanceOffset + window.scrollX)}px, ${e.clientY + (distanceOffset + window.scrollY)}px)`;
+    currX = e.clientX + distanceOffset;
+    currY = e.clientY + distanceOffset;
+
+});
+
+document.addEventListener('scroll', e => {
+    cursorBuddy.style.transform = `translate(${currX + window.scrollX}px, ${currY + window.scrollY}px)`;
+});
+
+
+
+
 let jeffry = document.getElementsByClassName("logo");
 
 
